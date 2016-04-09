e82a7f53e8eb6588f6550165aeef7c2de4d62713
wenjiangtao <wenjiangtao@outlook.com>
2016-04-09 15:42:39 +0800

    setting up the camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+    public Transform target;
+    public float smoothing = 5f;
+    
+    private Vector3 _offset;
+    
+    void Start()
+    {
+        _offset = transform.position - target.position;
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 targetCameraPos = target.position + _offset;
+        transform.position = Vector3.Lerp(transform.position, targetCameraPos, smoothing * Time.deltaTime);
+    }
+	
+}
